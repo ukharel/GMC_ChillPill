7d66a490ae70ca5a33e44e685ac7108b4c5112fb
Ujjwal <phoenixgene9271@gmail.com>
2026-07-24 14:51:05 +0545

    Added buttons and input

--- dynamic-food-rescue/tailwind.config.js
+++ dynamic-food-rescue/tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-      extend: {
-        colors: {
-          primary: "#0A8E5A",
-          secondary: "#F97316",
-        },
-      },
-    },
-    plugins: [],
-  }
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
